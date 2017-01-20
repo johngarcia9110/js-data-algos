f68f955e2f70b9beb84e9308d5b1b9c34cb81579
johngarcia9110 <johngarcia9110@gmail.com>
2017-01-19 19:41:37 -0500

    created folder for first exercise

---
+++ arrays/ex1.js
@@ -0,0 +1,8 @@
+/*
+
+Challange:
+
+Create a grades object that stores a set of student grades in an object.
+Provide a function for adding a grade and a function for displaying the student’s grade average.
+
+*/
